195c9645fb48008cd64252c73a5edacf6310867a
Almaz Azizov <105113852+s21violette@users.noreply.github.com>
2022-08-02 16:22:14 +0300

    Update Entry_Point.cpp

--- Model/Entry_Point.cpp
+++ Model/Entry_Point.cpp
@@ -2,13 +2,13 @@
 
 namespace s21 {
 
-    double Calc_Model::Get_Result() {
-        return Calculate();
-    }
+double Calc_Model::Get_Result() {
+    return Calculate();
+}
 
-    double Calc_Model::Start_Calc_Processing() {
-        Set_Data_To_Stack();
-        Polish_Notation();
-        return Get_Result();
-    }
+double Calc_Model::Start_Calc_Processing() {
+    Set_Data_To_Stack();
+    Polish_Notation();
+    return Get_Result();
+}
 }  // namespace s21
