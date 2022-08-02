dc07faa20cbc691a3d2d356ebf5ef2cdc57cd3f5
Almaz Azizov <105113852+s21violette@users.noreply.github.com>
2022-08-02 16:23:10 +0300

    Update Calc_Controller.cpp

--- Controller/Calc_Controller.cpp
+++ Controller/Calc_Controller.cpp
@@ -2,42 +2,42 @@
 
 namespace s21 {
 
-    Calc_Controller::Calc_Controller() {
-        Model_ = new Calc_Model;
-    }
+Calc_Controller::Calc_Controller() {
+    Model_ = new Calc_Model;
+}
 
-    Calc_Controller::~Calc_Controller() {
-        delete Model_;
-    }
+Calc_Controller::~Calc_Controller() {
+    delete Model_;
+}
 
-    double Calc_Controller::Get_Digit() {
-        return Model_->Start_Calc_Processing();
-    }
+double Calc_Controller::Get_Digit() {
+    return Model_->Start_Calc_Processing();
+}
 
-    double Calc_Controller::Get_Result_With_X() {
-        return Model_->Calculate();
-    }
+double Calc_Controller::Get_Result_With_X() {
+    return Model_->Calculate();
+}
 
-    bool Calc_Controller::Contains_X() {
-        return Model_->Contains_x_;
-    }
+bool Calc_Controller::Contains_X() {
+    return Model_->Contains_x_;
+}
 
-    void Calc_Controller::Set_X_Value(double val_x) {
-        Model_->x_.second = val_x;
-        Model_->x_.first = true;
-    }
+void Calc_Controller::Set_X_Value(double val_x) {
+    Model_->x_.second = val_x;
+    Model_->x_.first = true;
+}
 
-    bool Calc_Controller::Validate_Func_parameters(char *s1, char *s2, char *s3, char *s4) {
-        return Model_->Validate_Values(s1, s2, s3, s4);
-    }
+bool Calc_Controller::Validate_Func_parameters(char *s1, char *s2, char *s3, char *s4) {
+    return Model_->Validate_Values(s1, s2, s3, s4);
+}
 
-    int Calc_Controller::String_processing(const char *str) {
-        if (strlen(str) == 0 || strlen(str) > 255) return 1;
-        strcpy(default_str_, str);
-        if (strlen(default_str_) == 0 || strlen(default_str_) > 255)
-            return 1;
-        Model_->Remove_Spaces(default_str_);
-        Model_->Contains_x_ = (strchr(Model_->str_, 'x') != nullptr);
-        return Model_->Check_For_Errors();
-    }
+int Calc_Controller::String_processing(const char *str) {
+    if (strlen(str) == 0 || strlen(str) > 255) return 1;
+    strcpy(default_str_, str);
+    if (strlen(default_str_) == 0 || strlen(default_str_) > 255)
+        return 1;
+    Model_->Remove_Spaces(default_str_);
+    Model_->Contains_x_ = (strchr(Model_->str_, 'x') != nullptr);
+    return Model_->Check_For_Errors();
+}
 }  // namespace s21
